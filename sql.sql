ddf572ca03deb12aa9a4552a4bb7b7594f552e24
Olorunfemi Faith <115419444+Olorunfemi-Faith@users.noreply.github.com>
2026-07-06 13:58:56 +0100

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -26,6 +26,36 @@ CREATE TABLE ndvi (
     PRIMARY KEY (state, year, month)
 );
 
+
+
+
+-- ============================================================
+-- 2. DATA IMPORT (COPY STATEMENTS)
+-- ==========================================================
+
+-- Copy Rainfall Data
+COPY rainfall(state, year, month, actu_rfh, hist_rfh)
+FROM 'C:/mydata/rainfall_1.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Copy NDVI Data
+COPY ndvi(state, year, month, actu_vim, hist_vim)
+FROM 'C:/mydata/ndvi_1.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Copy Food Prices Data
+COPY food_prices(month, year, state, unit_value, unit_measure, price_per_kg)
+FROM 'C:/mydata/food_prices_1.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Copy CPI Data
+COPY cpi(year, cpi)
+FROM 'C:/mydata/cpi_1.csv'
+DELIMITER ','
+CSV HEADER;
 CREATE TABLE rainfall (
     state VARCHAR, 
     year INT, 
